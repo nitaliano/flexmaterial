5116a01b46579c281ea847b6c028a28920419adb
Nick Italiano <nitaliano@airware.com>
2016-01-03 20:20:48 -0800

    Removed type check from MutationObserver polyfill, since it is handled internally

--- src/flexmaterial.js
+++ src/flexmaterial.js
@@ -2,9 +2,7 @@
 require('./less/flexmaterial.less');
 
 // polyfills
-if (typeof MutationObserver === 'undefined') {
-    require('webcomponents.js/dist/MutationObserver');
-}
+require('webcomponents.js/dist/MutationObserver');
 
 // scripts
 require('./js/Button');
