8dd2bd893cdf829e44b09084ad5e2369f1919ddf
Nick Italiano <nitaliano@airware.com>
2016-01-03 21:09:06 -0800

    Added extra routes for components, and 404 page

--- tools/devserver.js
+++ tools/devserver.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 
 module.exports = function (cb) {
     var app = express();
@@ -11,8 +12,27 @@ module.exports = function (cb) {
 
     app.use('/static', express.static('dist'));
 
+    // default index
     app.get('/', function (req, res) {
         res.render('home');
+    });
+
+    // component routes
+    var components = fs.readdirSync(path.join(__dirname, '..', 'src', 'js'));
+    components.forEach(function (component) {
+       if (component.indexOf('.js') === -1) {
+           return;
+       }
+
+       var route = component.substring(0, component.length - 3);
+       app.get('/' + route, function (req, res) {
+           res.render(route);
+       });
+    });
+
+    app.get('*', function (req, res) {
+        res.status(404);
+        res.render('404');
     });
 
     app.listen(app.get('port'), function () {
