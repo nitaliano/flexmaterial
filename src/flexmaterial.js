54182ff96d42dc5729b3f0694a5238209ddf0b9e
Nick Italiano <nitaliano@airware.com>
2016-01-03 20:18:16 -0800

    Added MutationObserver polyfill from webcomponents.js

--- src/flexmaterial.js
+++ src/flexmaterial.js
@@ -1,5 +1,10 @@
 // styles
 require('./less/flexmaterial.less');
 
+// polyfills
+if (typeof MutationObserver === 'undefined') {
+    require('webcomponents.js/dist/MutationObserver');
+}
+
 // scripts
 require('./js/Button');
